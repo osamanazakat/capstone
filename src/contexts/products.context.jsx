da3fbad8ce47bc9547ce33f26b0fa8fe8e0737ba
osamanazakat <usamanazakat97@gmail.com>
2023-02-10 10:41:34 +0500

    adding cart context, shopping and dropdown

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -2,15 +2,16 @@ import { createContext, useState } from "react";
 
 import PRODUCTS from "../shop-data.json";
 
-const ProductContext = createContext({
+export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState(PRODUCTS);
-  const value = products;
-
+  const value = { products };
   return (
-    <ProductContext.Provider value={value}>{children}</ProductContext.Provider>
+    <ProductsContext.Provider value={value}>
+      {children}
+    </ProductsContext.Provider>
   );
 };
